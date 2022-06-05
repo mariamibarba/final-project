0da8a960e6e702f229128e17304bf56413e9d5f7
mariamibarba <73659597+mariamibarba@users.noreply.github.com>
2022-06-05 20:54:22 +0400

    add forms

--- app.js
+++ app.js
@@ -160,5 +160,60 @@ for (i = 0; i < projectsTitles.length; i++) {
   })
 }
 // 
+const signupForm = document.querySelector('#sign-up');
+const first_name = document.querySelector('#firstname');
+const email = document.querySelector('#email');
+const website = document.querySelector('#website');
+const message = document.querySelector('#message');
 
+signupForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const userData = {
+    first_name: first_name.value,
+    email: email.value,
+    website: website.value,
+    message: message.value,
+  };
+  createUser(userData);
+  signupForm.reset();
+ 
+});
+
+async function createUser(userData){
+  try {
+    const response = await fetch('http://api.kesho.me/v1/user-test/contact', {
+      method: 'POST',
+      body: JSON.stringify(userData),
+      headers: {'Content-Type': 'application/json'}
+    });
+    await response.json();
+   
+  }catch (e){
+    console.log('Error - ', e);
+  }
+}
+
+const modalOpenBtn = document.querySelector('#send-btn');
+const myModal = document.querySelector('.my-modal');
+
+
+modalOpenBtn.addEventListener('click', () => {
+  openModal('.my-modal');
+})
+
+function openModal(modalSelector){
+    const modalNode = document.querySelector(modalSelector);
+    modalNode.classList.add('visible');
+
+    const closeBtn = modalNode.querySelector('.modal-close');
+    closeBtn.addEventListener('click', () => {
+         closeModal(modalSelector)
+         
+  })
+}
+function closeModal(modalSelector){
+  const modalNode = document.querySelector(modalSelector);
+  modalNode.classList.remove('visible');
+  
+}
 
